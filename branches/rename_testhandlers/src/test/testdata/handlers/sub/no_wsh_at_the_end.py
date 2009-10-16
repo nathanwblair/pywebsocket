2d92ab3dec7494e7aedbb49d3aaa376c51f6e988
yuzo@chromium.org <yuzo@chromium.org@4ff78f4a-9131-11de-b045-6380ec9940d4>
2009-10-16 04:17:24 +0000

    Fix test handler messages

--- branches/rename_testhandlers/src/test/testdata/handlers/sub/no_wsh_at_the_end.py
+++ branches/rename_testhandlers/src/test/testdata/handlers/sub/no_wsh_at_the_end.py
@@ -38,7 +38,7 @@ def web_socket_do_extra_handshake(request):
 
 def web_socket_transfer_data(request):
     request.connection.write(
-            'sub/no_wsh_at_the_end_wsh.py is called for %s, %s' %
+            'sub/no_wsh_at_the_end.py is called for %s, %s' %
             (request.ws_resource, request.ws_protocol))
 
 
